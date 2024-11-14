86be252a57cf115af5c35ae19eba62e1ba7cfead
vusi <vusi@rqtech.co.za>
2024-11-14 23:10:59 +0200

    implementing proper scarfolding inline with MVC designs.

---
+++ Code/DeliciousCatering/src/main/java/com/vusieam/academics/deliciouscatering/domain/models/GenericResponse.java
@@ -0,0 +1,56 @@
+package com.vusieam.academics.deliciouscatering.domain.models;
+
+import java.util.List;
+
+/**
+ *
+ * @author vusi
+ */
+public class GenericResponse<T> {
+    private Integer code;
+    private Boolean status;
+    private String message;    
+    private List<T> dataList;
+    private T data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<T> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<T> dataList) {
+        this.dataList = dataList;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+    
+}
